b3da74dc9aed4e89048c68c3462e7e2468ff0952
piotr-nowoslawski-3mdeb <piotr.nowoslawski@3mdeb.com>
2021-02-10 17:37:27 +0100

    add navbar visible logic

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+
 const NavBar = (props) => {
-  return <nav className="navigation">{props.children}</nav>;
+  const [position, setPosition] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const offset = window.pageYOffset;
+    if (offset > position + 100) {
+      setPosition(offset);
+      setVisible(false);
+      setTimeout(() => setVisible(true), 300);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav className={`navigation ${!visible && "navigation__scroll"}`}>
+      {props.children}
+    </nav>
+  );
 };
 
 export default NavBar;
