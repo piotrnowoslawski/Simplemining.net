69b5475c8dfa3d9fff852ffa476f3a6fcc165090
piotr-nowoslawski-3mdeb <piotr.nowoslawski@3mdeb.com>
2021-02-10 22:42:33 +0100

    update css for faq elements

--- src/components/GroupItemTitle/GroupItemTitle.js
+++ src/components/GroupItemTitle/GroupItemTitle.js
@@ -1,7 +1,15 @@
+import { ImgElement } from "components";
+import icon from "images/icons/question.png";
+
 const GroupItemTitle = ({ title, handleClick }) => {
   return (
     <h3 className="group__title" onClick={handleClick}>
-      {title}
+      <ImgElement
+        iconSrc={icon}
+        text={"Title icon"}
+        styleName={"group__title-img"}
+      />
+      <span>{title}</span>
     </h3>
   );
 };
