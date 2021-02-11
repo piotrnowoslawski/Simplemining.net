3d1860fc4fae2454df14259e91242108c19b88ca
piotr-nowoslawski-3mdeb <piotr.nowoslawski@3mdeb.com>
2021-02-11 15:00:09 +0100

    scroll to # feature

--- src/components/GroupItem/GroupItem.js
+++ src/components/GroupItem/GroupItem.js
@@ -1,35 +1,51 @@
-import { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 import { GroupItemTitle, GroupItemContent } from "components";
 
 const GroupItem = ({ item }) => {
   const [isOpen, setOpen] = useState(false);
   const handleClick = () => setOpen(!isOpen);
+  const myRef = useRef();
+
+  const scrollTo = () => {
+    if (myRef.current) {
+      window.scrollTo({
+        behavior: "smooth",
+        top: myRef.current.offsetTop - 125,
+      });
+    }
+  };
 
   useEffect(() => {
-    window.location.hash === `#question-${item.id}`
-      ? setOpen(true)
-      : setOpen(false);
+    if (window.location.hash === `#question-${item.id}`) {
+      setOpen(true);
+      scrollTo();
+    } else {
+      setOpen(false);
+    }
   }, [window.location.hash]);
 
   useEffect(() => {
     window.addEventListener("hashchange", (e) => {
-      window.location.hash === `#question-${item.id}`
-        ? setOpen(true)
-        : setOpen(false);
+      if (window.location.hash === `#question-${item.id}`) {
+        setOpen(true);
+        scrollTo();
+      } else {
+        setOpen(false);
+      }
     });
   });
 
   return (
-    <li id={decodeURIComponent(item.title)}>
-      <NavLink
+    <li ref={myRef}>
+      <Link
         to={{
           pathname: "/",
           hash: `#question-${item.id}`,
         }}
       >
         <GroupItemTitle title={item.title} handleClick={handleClick} />
-      </NavLink>
+      </Link>
       <GroupItemContent content={item.content} isOpen={isOpen} />
     </li>
   );
